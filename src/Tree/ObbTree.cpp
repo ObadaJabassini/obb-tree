9a54b79faf70d36aa9d5fb4d4dcf38ba4966c7a0
Obada Jabassini <obj1995@hotmail.com>
2017-07-22 21:34:50 +0300

    Update ObbTree.cpp

--- src/Tree/ObbTree.cpp
+++ src/Tree/ObbTree.cpp
@@ -130,15 +130,10 @@ namespace Tree {
     }
 
     bool ObbTree::Node::intersect( OOBB& second ) {
-        for ( int i = 1; i <= 15; ++i ) {
-            if ( this->axisExists( this->obb, second, i )) {
-                return false;
-            }
-        }
-        return true;
+        return (!this->axisExists( this->obb, second));
     }
 
-    bool ObbTree::Node::axisExists( OOBB& first, OOBB& second, int caseNumber ) {
+    bool ObbTree::Node::axisExists( OOBB& first, OOBB& second) {
         Vector3 pa = first.center(), pb = second.center();
         Vector3 ax = first.getDirection( 0 ),
                 ay = first.getDirection( 1 ),
@@ -161,69 +156,71 @@ namespace Tree {
                 hb = (secondCorners.at( 0 ) - secondCorners.at( 2 )).norm() / 2,
                 db = (secondCorners.at( 0 ) - secondCorners.at( 4 )).norm() / 2;
         Vector3 t = (pb - pa);
-        switch ( caseNumber ) {
-            case 1: {
-                auto temp = t.dot( ax );
-                return abs( temp ) > wa + wb * abs( ax.dot( bx )) + hb * abs( ax.dot( by )) + db * abs( ax.dot( bz ));
+        for(int i = 1; i <= 15; ++i){
+            switch ( i ) {
+                case 1: {
+                    auto temp = t.dot( ax );
+                    return abs( temp ) > wa + wb * abs( ax.dot( bx )) + hb * abs( ax.dot( by )) + db * abs( ax.dot( bz ));
+                }
+                case 2: {
+                    auto temp2 = t.dot( ay );
+                    return abs( temp2 ) > ha + wb * abs( ay.dot( bx )) + hb * abs( ay.dot( by )) + db * abs( ay.dot( bz ));
+                }
+                case 3: {
+                    auto temp3 = t.dot( az );
+                    return abs( temp3 ) > da + wb * abs( az.dot( bx )) + hb * abs( az.dot( by )) + db * abs( az.dot( bz ));
+                }
+                case 4: {
+                    auto temp4 = t.dot( bx );
+                    return abs( temp4 ) > wa * abs( ax.dot( bx )) + ha * abs( ay.dot( bx )) + da * abs( az.dot( bx )) + wb;
+                }
+                case 5: {
+                    auto temp5 = t.dot( by );
+                    return abs( temp5 ) > wa * abs( ax.dot( by )) + ha * abs( ay.dot( by )) + da * abs( az.dot( by )) + hb;
+                }
+                case 6: {
+                    auto temp6 = t.dot( bz );
+                    return abs( temp6 ) > wa * abs( ax.dot( bz )) + ha * abs( ay.dot( bz )) + da * abs( az.dot( bz )) + db;
+                }
+                case 7:
+                    return abs( t.dot( az ) * ay.dot( bx ) - t.dot( ay ) * az.dot( bx )) >
+                           ha * abs( ax.dot( bx )) + da * abs( ay.dot( bx )) + hb * abs( ax.dot( bz )) +
+                           db * abs( ax.dot( by ));
+                case 8:
+                    return abs( t.dot( az ) * ay.dot( by ) - t.dot( ay ) * az.dot( by )) >
+                           ha * abs( az.dot( by )) + da * abs( ay.dot( by )) + wb * abs( ax.dot( bz )) +
+                           db * abs( ax.dot( bx ));
+                case 9:
+                    return abs( t.dot( az ) * ay.dot( bz ) - t.dot( ay ) * az.dot( bz )) >
+                           ha * abs( az.dot( bz )) + da * abs( ay.dot( bz )) + wb * abs( ax.dot( by )) +
+                           hb * abs( ax.dot( bx ));
+                case 10:
+                    return abs( t.dot( ax ) * ax.dot( bx ) - t.dot( az ) * ax.dot( bx )) >
+                           wa * abs( az.dot( bx )) + da * abs( ax.dot( bx )) + hb * abs( ay.dot( bz )) +
+                           db * abs( ay.dot( by ));
+                case 11:
+                    return abs( t.dot( ax ) * az.dot( by ) - t.dot( ax ) * ax.dot( by )) >
+                           wa * abs( az.dot( by )) + da * abs( ax.dot( by )) + wb * abs( ay.dot( bz )) +
+                           db * abs( ay.dot( bx ));
+                case 12:
+                    return abs( t.dot( ax ) * az.dot( bz ) - t.dot( az ) * ax.dot( bz )) >
+                           wa * abs( az.dot( bz )) + da * abs( ax.dot( bz )) + wb * abs( ay.dot( by )) +
+                           hb * abs( ay.dot( bx ));
+                case 13:
+                    return abs( t.dot( ay ) * ax.dot( bx ) - t.dot( ax ) * ay.dot( bx )) >
+                           wa * abs( ay.dot( by )) + ha * abs( ax.dot( bx )) + hb * abs( az.dot( az )) +
+                           db * abs( az.dot( by ));
+                case 14:
+                    return abs( t.dot( ay ) * ax.dot( by ) - t.dot( ax ) * ay.dot( by )) >
+                           wa * abs( ay.dot( by )) + ha * abs( ax.dot( by )) + wb * abs( az.dot( bz )) +
+                           db * abs( az.dot( bx ));
+                case 15:
+                    return abs( t.dot( ay ) * ax.dot( bz ) - t.dot( ax ) * ay.dot( bz )) >
+                           wa * abs( ay.dot( bz )) + ha * abs( ax.dot( bz )) + wb * abs( az.dot( by )) +
+                           hb * abs( az.dot( bx ));
+                default:
+                    return false;
             }
-            case 2: {
-                auto temp2 = t.dot( ay );
-                return abs( temp2 ) > ha + wb * abs( ay.dot( bx )) + hb * abs( ay.dot( by )) + db * abs( ay.dot( bz ));
-            }
-            case 3: {
-                auto temp3 = t.dot( az );
-                return abs( temp3 ) > da + wb * abs( az.dot( bx )) + hb * abs( az.dot( by )) + db * abs( az.dot( bz ));
-            }
-            case 4: {
-                auto temp4 = t.dot( bx );
-                return abs( temp4 ) > wa * abs( ax.dot( bx )) + ha * abs( ay.dot( bx )) + da * abs( az.dot( bx )) + wb;
-            }
-            case 5: {
-                auto temp5 = t.dot( by );
-                return abs( temp5 ) > wa * abs( ax.dot( by )) + ha * abs( ay.dot( by )) + da * abs( az.dot( by )) + hb;
-            }
-            case 6: {
-                auto temp6 = t.dot( bz );
-                return abs( temp6 ) > wa * abs( ax.dot( bz )) + ha * abs( ay.dot( bz )) + da * abs( az.dot( bz )) + db;
-            }
-            case 7:
-                return abs( t.dot( az ) * ay.dot( bx ) - t.dot( ay ) * az.dot( bx )) >
-                       ha * abs( ax.dot( bx )) + da * abs( ay.dot( bx )) + hb * abs( ax.dot( bz )) +
-                       db * abs( ax.dot( by ));
-            case 8:
-                return abs( t.dot( az ) * ay.dot( by ) - t.dot( ay ) * az.dot( by )) >
-                       ha * abs( az.dot( by )) + da * abs( ay.dot( by )) + wb * abs( ax.dot( bz )) +
-                       db * abs( ax.dot( bx ));
-            case 9:
-                return abs( t.dot( az ) * ay.dot( bz ) - t.dot( ay ) * az.dot( bz )) >
-                       ha * abs( az.dot( bz )) + da * abs( ay.dot( bz )) + wb * abs( ax.dot( by )) +
-                       hb * abs( ax.dot( bx ));
-            case 10:
-                return abs( t.dot( ax ) * ax.dot( bx ) - t.dot( az ) * ax.dot( bx )) >
-                       wa * abs( az.dot( bx )) + da * abs( ax.dot( bx )) + hb * abs( ay.dot( bz )) +
-                       db * abs( ay.dot( by ));
-            case 11:
-                return abs( t.dot( ax ) * az.dot( by ) - t.dot( ax ) * ax.dot( by )) >
-                       wa * abs( az.dot( by )) + da * abs( ax.dot( by )) + wb * abs( ay.dot( bz )) +
-                       db * abs( ay.dot( bx ));
-            case 12:
-                return abs( t.dot( ax ) * az.dot( bz ) - t.dot( az ) * ax.dot( bz )) >
-                       wa * abs( az.dot( bz )) + da * abs( ax.dot( bz )) + wb * abs( ay.dot( by )) +
-                       hb * abs( ay.dot( bx ));
-            case 13:
-                return abs( t.dot( ay ) * ax.dot( bx ) - t.dot( ax ) * ay.dot( bx )) >
-                       wa * abs( ay.dot( by )) + ha * abs( ax.dot( bx )) + hb * abs( az.dot( az )) +
-                       db * abs( az.dot( by ));
-            case 14:
-                return abs( t.dot( ay ) * ax.dot( by ) - t.dot( ax ) * ay.dot( by )) >
-                       wa * abs( ay.dot( by )) + ha * abs( ax.dot( by )) + wb * abs( az.dot( bz )) +
-                       db * abs( az.dot( bx ));
-            case 15:
-                return abs( t.dot( ay ) * ax.dot( bz ) - t.dot( ax ) * ay.dot( bz )) >
-                       wa * abs( ay.dot( bz )) + ha * abs( ax.dot( bz )) + wb * abs( az.dot( by )) +
-                       hb * abs( az.dot( bx ));
-            default:
-                return false;
         }
     }
 
